f99868c981651726fc2b86e2f767307411d469d2
Logon27 <logondz27@gmail.com>
2022-12-04 18:52:39 -0500

    Refactor TrainingSet Variable Names And Remove Model Helper
    Removing the model helper class. Refactored the training set and network variable names to be more descriptive

--- nn/network/model.py
+++
@@ -1,3 +0,0 @@
-def create_model(layers, learning_rate, weight_initializer, optimizer, optimizerArgs={}):
-    # This is just going to be used as a helper to initialize weights for an entire model instead of per layer.
-    pass
